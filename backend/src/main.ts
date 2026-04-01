4acd3dc49a840047e21a6cd540fe685293def537
leancarr <leandrocaraballo2oo7@gmail.com>
2026-04-01 15:29:43 -0300

    Frontend final

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,8 +4,15 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe()); // agregá esta línea
-  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: [
+      'http://localhost:3000', 
+      'https://lambda-works-test-3kzpdy0y5-leancarrs-projects.vercel.app'
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   await app.listen(4000);
 }
 bootstrap();
